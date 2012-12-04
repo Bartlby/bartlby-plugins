b55e9e9361f4a205baf93b578818e67edff3b86e
Helmut Januschka <helmut@januschka.com>
2012-12-04 07:38:52 +0100

    fixed regex function not compatible anymore Signed-off-by: hjanuschka <helmut@januschka.com>

--- bartlby_check_http.c
+++ bartlby_check_http.c
@@ -41,7 +41,7 @@ int timing;
 int my_regMatch(char * subj, char * pat) {
 	int iResult;	
 	regex_t reg;
-	if(regcomp(&reg, pat, RE_SYNTAX_EGREP) != 0) {
+	if(regcomp(&reg, pat, REG_EXTENDED) != 0) {
 		return -1;
 	}
 	iResult = regexec(&reg, subj,0 ,0,0);
