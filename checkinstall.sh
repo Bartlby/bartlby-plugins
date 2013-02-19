631719eb1bc2242327ed255331a7953a960bc197
Helmut Januschka <helmut@januschka.com>
2013-02-19 19:51:40 +0100

    checkinstall for plugins

---
+++ checkinstall.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+echo "Generating Debian Package";
+
+if [ "x$1" = "x" ];
+then
+	echo -n -e "Version:";
+	read v;
+else
+	v=$1;
+fi;
+
+
+
+checkinstall --install=no --pkgversion=$v --nodoc --maintainer=helmut@januschka.com --pkgname=bartlby-plugins  -y 
